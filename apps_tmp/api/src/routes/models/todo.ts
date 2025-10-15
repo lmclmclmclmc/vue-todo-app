5916814816129787d50f112a79fa476978c001bf
lmclmclmclmc <3148422587@qq.com>
2025-10-15 13:40:47 +0800

    fix: rename apps_tmp to apps for render deployment

--- apps_tmp/api/src/routes/models/todo.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const TodoSchema = new mongoose.Schema({
-  text:{type:String,required:true},
-  done:{type:Boolean,default:false},
-
-},{timestamps:true})
-
-export default mongoose.model('Todo', TodoSchema)
